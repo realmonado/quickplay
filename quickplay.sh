b10bf7e01000debfe12274c1fd8409460b496d7d
monado <monado@snopyta.org>
2020-09-06 23:05:38 -0400

    added ability to loop, got rid of commit.sh from the repo

--- quickplay.sh
+++ quickplay.sh
@@ -2,9 +2,16 @@ echo "enter your search:"
 read msg
 echo "do you want to keep the file? [yes/no]"
 read choice
+echo "do you want to loop over the file [yes/no]"
+read choice2
 youtube-dl "ytsearch: $msg" -o '%(title)s.%(ext)s' --format mp4
 vid=$(youtube-dl "ytsearch: $msg" --get-filename -o '%(title)s.%(ext)s'  --format mp4)
-mpv "$vid"
+if [ "$choice2" = "yes" ]
+then
+   mpv "$vid" --loop
+else
+   mpv "$vid"
+fi
 
 if [ "$choice" = "yes" ]
 then
